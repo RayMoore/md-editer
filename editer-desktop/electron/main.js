be1ac50155de863525adb06f3c41476429f8059a
e <l>
2020-06-11 11:51:27 +0800

    first release

--- editer-desktop/electron/main.js
+++ editer-desktop/electron/main.js
@@ -1,14 +1,14 @@
 const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require('path');
 const menuBuilder = require("./menu-builder");
-const TITLE = "Cokee";
+const title = "Cokee";
 let mainWindow = null;
 let windowOption = {
   width: 1440,
   height: 768,
   minWidth: 1024,
   minHeight: 600,
-  title: TITLE,
+  title,
   center: true,
   webPreferences: {
     nodeIntegration: true,
@@ -23,19 +23,27 @@ const createWindow = () => {
   mainWindow.loadURL(urlLocation);
   // default menu
   const locale = app.getLocale();
-  const menu = Menu.buildFromTemplate(menuBuilder(locale));
+  const menu = Menu.buildFromTemplate(menuBuilder(locale, false));
   Menu.setApplicationMenu(menu);
   ipcMain.on("locale-changed", (event, args) => {
     // menu rebuild on locale change
-    const newMenu = Menu.buildFromTemplate(menuBuilder(args));
+    const newMenu = Menu.buildFromTemplate(menuBuilder(args, false));
     Menu.setApplicationMenu(newMenu);
   });
+  ipcMain.on('close-app', ()=>{
+    ipcMain.removeAllListeners();
+    mainWindow.destroy();
+  })
   if(process.platform === 'win32'){
     mainWindow.setIcon(path.win32.join(__dirname, "static", "icon.ico"));
   }
   mainWindow.on('ready-to-show', function(){
     mainWindow.show();
   });
+  mainWindow.on('close', function(event){
+    event.preventDefault();
+    mainWindow.webContents.send('app-will-close');
+  })
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
